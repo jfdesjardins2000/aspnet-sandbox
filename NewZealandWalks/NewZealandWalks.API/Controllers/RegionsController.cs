31a4dc8617d748e6571b86d58dc8fe45d7cb1812
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2024-12-11 19:20:31 -0500

    ajout de RegionsController.cs

---
+++ NewZealandWalks/NewZealandWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewZealandWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+    }
+}
