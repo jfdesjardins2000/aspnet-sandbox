23dfc5e69314bac57d4450eed7ee2b3655c62b1d
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2024-12-17 21:37:13 -0500

    Ajout des mapping domain model <-> dto

---
+++ NewZealandWalks/NewZealandWalks.API/Models/Mapping/DomainToDtoMapper.cs
@@ -0,0 +1,23 @@
+﻿using NewZealandWalks.API.Models.Domain;
+using NewZealandWalks.API.Models.DTO;
+
+namespace NewZealandWalks.API.Models.Mapping
+{
+    public static class DomainToDtoMapper
+    {
+        public static RegionDto ToRegionDto(this Region region)
+        {
+            RegionDto regionDto =
+                new RegionDto()
+                {
+                    Id = region.Id,
+                    Name = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl
+                };
+
+            return regionDto;
+        }
+
+    }
+}
---
+++ NewZealandWalks/NewZealandWalks.API/Models/Mapping/DtoToDomainMapper.cs
@@ -0,0 +1,22 @@
+﻿using NewZealandWalks.API.Models.Domain;
+using NewZealandWalks.API.Models.DTO;
+
+namespace NewZealandWalks.API.Models.Mapping
+{
+    public static class DtoToDomainMapper
+    {
+        public static Region ToRegionDto(this RegionDto regionDto)
+        {
+            Region region =
+                new Region()
+                {
+                    Id = regionDto.Id,
+                    Name = regionDto.Name,
+                    Code = regionDto.Code,
+                    RegionImageUrl = regionDto.RegionImageUrl
+                };
+
+            return region;
+        }
+    }
+}
