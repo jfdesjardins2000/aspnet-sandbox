5af3d2602ffc12f9724151a0d251421a2a001896
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2024-12-28 18:14:19 -0500

    Modif dans SQLWalkRepository pour permettre de filtrer, trier, paginer...

--- NewZealandWalks/NewZealandWalks.API/Repositories/SQLWalkRepository.cs
+++ NewZealandWalks/NewZealandWalks.API/Repositories/SQLWalkRepository.cs
@@ -34,8 +34,12 @@ namespace NewZealandWalks.API.Repositories
             return existingWalk;
         }
 
-        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null,
-            string? sortBy = null, bool isAscending = true, int pageNumber = 1, int pageSize = 1000)
+        public async Task<List<Walk>> GetAllAsync(string? filterOn = null,
+                                                    string? filterQuery = null,
+                                                    string? sortBy = null,
+                                                    bool isAscending = true,
+                                                    int pageNumber = 1,
+                                                    int pageSize = 1000)
         {
             var walks = _dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable();
 
