7bc980106866891a26128786dbf59044c41f6146
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2024-12-28 19:25:33 -0500

    ajout de l'attribut [Authorize]

--- NewZealandWalks/NewZealandWalks.API/Controllers/RegionsController.cs
+++ NewZealandWalks/NewZealandWalks.API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,7 @@ namespace NewZealandWalks.API.Controllers
     // https://localhost:1234/api/regions
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class RegionsController : ControllerBase
     {
         private readonly NZWalksDbContext _dbContext;
