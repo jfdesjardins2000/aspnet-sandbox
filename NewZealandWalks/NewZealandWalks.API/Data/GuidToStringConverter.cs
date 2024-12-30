7b5567d0815cf5475dfac0996cac5e9521ca7b59
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2024-12-29 21:30:53 -0500

    Ajout de GuidToStringConverter.cs qui permet de convertir du type GUID dans les classes C# vers le type TEXT des tables Sqlite.

---
+++ NewZealandWalks/NewZealandWalks.API/Data/GuidToStringConverter.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace NewZealandWalks.API.Data
+{
+    public class GuidToStringConverter : ValueConverter<Guid, string>
+    {
+        public GuidToStringConverter() : base(g => g.ToString(), s => new Guid(s))
+        {
+        }
+    }
+}
