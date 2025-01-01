1ef9d311a7d8ed325aab3be00f035ee3693e524c
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2025-01-01 13:04:21 -0500

    ajout de ImagesController.cs

---
+++ NewZealandWalks/NewZealandWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewZealandWalks.API.Controllers
+{
+    // https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController
+    {
+    }
+}
