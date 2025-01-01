0556eabcd1dbf397ff1d8c236c212b1190bb3ce4
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2025-01-01 13:32:34 -0500

    ajout de LocalImageRepository.cs

---
+++ NewZealandWalks/NewZealandWalks.API/Repositories/LocalImageRepository.cs
@@ -0,0 +1,12 @@
+﻿using NewZealandWalks.API.Models.Domain;
+
+namespace NewZealandWalks.API.Repositories
+{
+    public class LocalImageRepository : IImageRepository
+    {
+        public Task<Image> Upload(Image image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
