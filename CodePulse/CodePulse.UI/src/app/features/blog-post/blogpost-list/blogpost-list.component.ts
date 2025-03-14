fe87cdc52a946e32887cc01f2beda49ee62e1f10
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2025-03-14 14:07:04 -0400

    ajout de blog-post/blogpost-list/blogpost-list.component.*

---
+++ CodePulse/CodePulse.UI/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogpost-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './blogpost-list.component.html',
+  styleUrl: './blogpost-list.component.css'
+})
+export class BlogpostListComponent {
+
+}
