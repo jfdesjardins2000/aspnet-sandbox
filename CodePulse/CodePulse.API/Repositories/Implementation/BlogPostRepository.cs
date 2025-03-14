eb64fc2113e9f02072d6dcc4b108721643f26844
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2025-03-14 15:23:25 -0400

    modif de CodePulse/CodePulse.API/Repositories/Implementation/BlogPostRepository.cs

--- CodePulse/CodePulse.API/Repositories/Implementation/BlogPostRepository.cs
+++ CodePulse/CodePulse.API/Repositories/Implementation/BlogPostRepository.cs
@@ -1,38 +1,74 @@
-﻿using CodePulse.API.Models.Domain;
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
 using CodePulse.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodePulse.API.Repositories.Implementation
 {
     public class BlogPostRepository : IBlogPostRepository
     {
-        public Task<BlogPost> CreateAsync(BlogPost blogPost)
+        private readonly ApplicationDbContext dbContext;
+
+        public BlogPostRepository(ApplicationDbContext dbContext)
         {
-            throw new NotImplementedException();
+            this.dbContext = dbContext;
         }
 
-        public Task<BlogPost?> DeleteAsync(Guid id)
+        public async Task<BlogPost> CreateAsync(BlogPost blogPost)
         {
-            throw new NotImplementedException();
+            await dbContext.BlogPosts.AddAsync(blogPost);
+            await dbContext.SaveChangesAsync();
+            return blogPost;
         }
 
-        public Task<IEnumerable<BlogPost>> GetAllAsync()
+        public async Task<BlogPost?> DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var existingBlogPost = await dbContext.BlogPosts.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingBlogPost != null)
+            {
+                dbContext.BlogPosts.Remove(existingBlogPost);
+                await dbContext.SaveChangesAsync();
+                return existingBlogPost;
+            }
+
+            return null;
         }
 
-        public Task<BlogPost?> GetByIdAsync(Guid id)
+        public async Task<IEnumerable<BlogPost>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await dbContext.BlogPosts.Include(x => x.Categories).ToListAsync();
         }
 
-        public Task<BlogPost?> GetByUrlHandleAsync(string urlHandle)
+        public async Task<BlogPost?> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await dbContext.BlogPosts.Include(x => x.Categories).FirstOrDefaultAsync(x => x.Id == id);
         }
 
-        public Task<BlogPost?> UpdateAsync(BlogPost blogPost)
+        public async Task<BlogPost?> GetByUrlHandleAsync(string urlHandle)
         {
-            throw new NotImplementedException();
+            return await dbContext.BlogPosts.Include(x => x.Categories).FirstOrDefaultAsync(x => x.UrlHandle == urlHandle);
+        }
+
+        public async Task<BlogPost?> UpdateAsync(BlogPost blogPost)
+        {
+            var existingBlogPost = await dbContext.BlogPosts.Include(x => x.Categories)
+                .FirstOrDefaultAsync(x => x.Id == blogPost.Id);
+
+            if (existingBlogPost == null)
+            {
+                return null;
+            }
+
+            // Update BlogPost
+            dbContext.Entry(existingBlogPost).CurrentValues.SetValues(blogPost);
+
+            // Update Categories
+            existingBlogPost.Categories = blogPost.Categories;
+
+            await dbContext.SaveChangesAsync();
+
+            return blogPost;
         }
     }
 }
