4357c4b3e9eb7c2d9262d02fdcbc363f7583c788
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2025-03-18 16:48:35 -0400

    ajout de app/features/blog-post/edit-blogpost/edit-blogpost.component.*

---
+++ CodePulse/CodePulse.UI/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-blogpost',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-blogpost.component.html',
+  styleUrl: './edit-blogpost.component.css'
+})
+export class EditBlogpostComponent {
+
+}
