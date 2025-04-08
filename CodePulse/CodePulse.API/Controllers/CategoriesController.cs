39bf6a17cb8dbaf0a926c08e2687ec61ef94b1ad
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2025-04-08 18:28:02 -0400

    modif pour controller l'acces au fonctions des controleur avec les ROLES

--- CodePulse/CodePulse.API/Controllers/CategoriesController.cs
+++ CodePulse/CodePulse.API/Controllers/CategoriesController.cs
@@ -20,7 +20,7 @@ namespace CodePulse.API.Controllers
         }
 
         [HttpPost]
-        //[Authorize(Roles = "Writer")]
+        [Authorize(Roles = "Writer")]
         public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryRequestDto request)
         {
             // Map DTO to Domain Model
@@ -45,7 +45,7 @@ namespace CodePulse.API.Controllers
 
         // GET: https://localhost:7015/api/Categories?query=html&sortBy=name&sortDirection=desc
         [HttpGet]
-        [Authorize]
+        [Authorize(Roles = "Reader")]
         public async Task<IActionResult> GetAllCategories(
             [FromQuery] string? query,
             [FromQuery] string? sortBy,
@@ -97,7 +97,7 @@ namespace CodePulse.API.Controllers
         // PUT: https://localhost:7015/api/categories/{id}
         [HttpPut]
         [Route("{id:Guid}")]
-        //[Authorize(Roles = "Writer")]
+        [Authorize(Roles = "Writer")]
         public async Task<IActionResult> EditCategory([FromRoute] Guid id, UpdateCategoryRequestDto request)
         {
             // Convert DTO to Domain Model
@@ -129,7 +129,7 @@ namespace CodePulse.API.Controllers
         // DELETE: https://localhost:7015/api/categories/{id}
         [HttpDelete]
         [Route("{id:Guid}")]
-        //[Authorize(Roles = "Writer")]
+        [Authorize(Roles = "Writer")]
         public async Task<IActionResult> DeleteCategory([FromRoute] Guid id)
         {
             var category = await categoryRepository.DeleteAsync(id);
