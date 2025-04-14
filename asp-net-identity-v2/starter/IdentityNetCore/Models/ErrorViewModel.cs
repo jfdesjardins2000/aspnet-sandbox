d9d5688413f9d6faa7551ae2d679070d4521fd53
Desjardins <Jean-Francois.Desjardins@environnement.gouv.qc.ca>
2025-04-14 11:53:28 -0400

    Supprimer le dossier asp-net-identity-v2

--- asp-net-identity-v2/starter/IdentityNetCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IdentityNetCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- asp-net-identity-v2/starter/IdentityNetCore/Services/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace IdentityNetCore.Services
-{
-    public interface IEmailSender
-    {
-        Task SendEmailAsync(string fromAddress, string toAddress, string subject, string message);
-    }
-}
