e4634041d1f8e48918f428e4d54522770bd50ad5
Desjardins <Jean-Francois.Desjardins@environnement.gouv.qc.ca>
2025-04-16 17:30:33 -0400

    suppresion de CodePulse.API-master

--- prof/CodePulse.API-master/CodePulse.API/Repositories/Interface/ITokenRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace CodePulse.API.Repositories.Interface
-{
-    public interface ITokenRepository
-    {
-        string CreateJwtToken(IdentityUser user, List<string> roles);
-    }
-}
